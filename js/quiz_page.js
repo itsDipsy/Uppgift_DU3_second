45c254463e938ed2f4487f22ae64abff46173a04
itsDipsy <paregos55.collins@gmail.com>
2023-02-16 21:38:03 +0100

    Update quiz_page.js

--- js/quiz_page.js
+++ js/quiz_page.js
@@ -1,22 +1,4 @@
 function init_quiz_page_html_component() {
-<<<<<<< Updated upstream
-    document.body.style.backgroundColor = "turquoise";
-    document.querySelector("#the_whole").innerHTML = "";
-    let html_component_quiz_page = document.querySelector("#the_whole").innerHTML = `
-    <header>
-        <nav>
-            <li>
-                <img class="dog_logo" src="./media/logo.png" alt="">
-            </li>
-            <li>log out</li>
-        </nav>
-    </header>
-
-    <div class="page_name">Login</div>
-
-    <div id="quiz_page_quiz_wrapper">
-
-=======
 
     document.body.style.backgroundImage = "linear-gradient(180deg, rgba(25,25,25,0), rgba(25,25,140,1))";
     document.body.style.backgroundRepeat = "no-repeat";
@@ -52,16 +34,11 @@ function init_quiz_page_html_component() {
             <div class="quiz_box" id="box3"></div>   
             <div class="quiz_box" id="box4"></div>   
         </div>
->>>>>>> Stashed changes
     </div>
 
     <footer>
         <p>The Dog Breed Quiz is made possible thanks to the free API by <a href="">DOG CEO Zinc</a> </p>
-<<<<<<< Updated upstream
-    </footer>
-=======
     </footer>   
->>>>>>> Stashed changes
 `
 
 the_quiz();
