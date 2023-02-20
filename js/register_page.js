90ad989b4abc86423348c7cf45ed4bd698864535
itsDipsy <paregos55.collins@gmail.com>
2023-02-20 12:35:03 +0100

    Update register_page.js

--- js/register_page.js
+++ js/register_page.js
@@ -5,7 +5,7 @@ function register() {
     
     document.querySelector("body").style.backgroundColor = "green";
     document.body.style.backgroundImage = "linear-gradient(#e66465, #9198e5)";
-    document.querySelector(".page_name").innerHTML = "Request"
+    document.querySelector(".page_name").innerHTML = "Register"
 
     document.querySelector("#new").innerHTML = "Already have a account? login in"
     document.querySelector(".slogan").innerHTML = "Let the quiz games begin";
