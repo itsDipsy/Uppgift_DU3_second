85aefc9d67ac9f02efb7c14e07f1c61e1e37032d
itsDipsy <paregos55.collins@gmail.com>
2023-02-21 18:47:04 +0100

    Update register_page.js

--- js/register_page.js
+++ js/register_page.js
@@ -1,4 +1,6 @@
 function register() {
+
+    document.body.style.backgroundImage = "linear-gradient(#e66465, #9198e5)";
     
     document.querySelector(".page_name").innerHTML = "Request"
 
@@ -13,15 +15,20 @@ function register() {
     function reister_function() {
         if (document.querySelector("#new").classList.contains("selected") === true) {
             document.querySelector(".register_button").addEventListener("click", (event) => {
-                
                 event.stopImmediatePropagation(); // Denna behövs för annars kommer både login och fetch click event bubblas
+
+
+                start_request("post");
+                document.querySelector(".username").value = ""
+                document.querySelector(".password").value = ""
+    
 
                  async function test(){
                     let request_server_dom = document.createElement("div");
                     request_server_dom.classList.add("the_await_request_dom");
                     document.querySelector("#the_whole").appendChild(request_server_dom);
                     request_server_dom.innerHTML = `
-                        <div>fetching server ...</div>
+                        <div>fetching image ...</div>
                     `;
 
 
@@ -33,6 +40,7 @@ function register() {
                 test();
                 document.querySelector(".username").value = "";
                 document.querySelector(".password").value = "";
+
             })
         }
     }
